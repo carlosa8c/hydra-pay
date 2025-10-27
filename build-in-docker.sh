c1674bb3c1739c0aed381c93ddebf1e770986fe9
Carlos Cabrera <carlos@tumblr.com>
2025-10-26 23:19:36 -0400

    build: stop uninstalling git-minimal in docker helper

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -153,7 +153,6 @@ docker run --rm \
   -e HTTP_PROXY="$HTTP_PROXY" -e http_proxy="$http_proxy" \
   nixos/nix \
   bash -lc "set -Eeuo pipefail; \
-    if nix-env -q git-minimal >/dev/null 2>&1; then nix-env -e git-minimal >/dev/null; fi; \
     nix-env -iA nixpkgs.nix-prefetch-scripts nixpkgs.cabal2nix nixpkgs.git nixpkgs.cacert >/dev/null; \
   if [ -d /root/.nix-profile/bin ]; then export PATH="/root/.nix-profile/bin:$PATH"; fi; \
     mkdir -p /etc/nix; \
